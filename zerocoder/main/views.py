f7ddbf96e7831454e87398272a18bed6de13ef62
Anna <nikoshina.anna@gmail.com>
2025-06-22 15:00:35 +0300

    dj02_l1

--- zerocoder/main/views.py
+++ zerocoder/main/views.py
@@ -1,13 +1,14 @@
+from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("<h1>Это мой первый проект на Django</h1>")
+    return render(request, 'main/index.html')
 
 def new(request):
-    return HttpResponse("<h1>Это вторая страница моего проекта на Django</h1>")
+    return render(request, 'main/new.html')
 
-def data(request):
-    return HttpResponse("<h1>Это третья страница моего проекта на Django, которая называется data.</h1>")
-
-def test(request):
-    return HttpResponse("<h1>Это четвертая страница моего проекта на Django, которая называется test.</h1>")
+# def data(request):
+#     return HttpResponse("<h1>Это третья страница моего проекта на Django, которая называется data.</h1>")
+#
+# def test(request):
+#     return HttpResponse("<h1>Это четвертая страница моего проекта на Django, которая называется test.</h1>")
